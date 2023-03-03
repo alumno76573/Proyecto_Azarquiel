dc6154a9ef3bdf7fe986798453bd6493eaee3bfd
alumno765731~ <alumno.76573@ies-azarquiel.es>
2023-03-03 16:50:27 +0100

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
 	}
